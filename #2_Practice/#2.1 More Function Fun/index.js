4dd7a856ffe6133ed13c7bbde634767cf1d02b3c
HANSOL <yhansol145@gmail.com>
2021-02-24 16:56:07 +0900

    #2.1 More Function Fun

--- #2_Practice/#2.1 More Function Fun/index.js
+++ #2_Practice/#2.1 More Function Fun/index.js
@@ -1,17 +1,35 @@
-function sayHello(name, age){
-    console.log('Hello!', name, " you have ", age, " years of age.");
+const calculator = {
+    plus: function(a, b){
+        return a + b;
+    },
+    minus: function(a, b){
+        return a - b;
+    },
+    multiple: function(a, b){
+        return a * b;
+    },
+    divide: function(a, b){
+        return a / b;
+    },
+    power: function(a, b){
+        return a ** b;
+    }
+
 }
 
+const plus = calculator.plus(3890, 7716)
+console.log(plus)
 
-sayHello("Hansol", 29);
+const minus = calculator.minus(17223, 7899)
+console.log(minus)
 
-/*
-- console은 object다.
-- console.log, alert 등을 내장함수(built-in function)이라고 한다.
-- 함수는 어떤 걸 수행하려는 한 부분으로, 원하는 만큼 쓸 수 있다.
-- 함수를 쓸 땐, funcition 함수명() {}
-- log도 함수인데, console object 안에 있는 함수라는 의미이다.
-- 함수명() 괄호 안에 들어가는 단어를 인자(argument)라고 한다. 또는 parameter.
-- argument는 변수와 비슷하다. 함수 안에서 사용하게 될 이름이다.
-- argument를 사용하는 것이 외부에 있는 데이터를 읽는 함수를 만드는 방법이다.
-*/
+const multiple = calculator.multiple(88, 19978)
+console.log(multiple)
+
+const divide = calculator.divide(2222, 11)
+console.log(divide)
+
+const power = calculator.power(44, 2)
+console.log(power)
+
+// 객체를 이용하여 계산기 활용하기
