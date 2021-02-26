935dad478b85ed277c30f35f67b440e0d9d84261
HANSOL <yhansol145@gmail.com>
2021-02-26 13:09:19 +0900

    #2.7 DOM - If else - Function practive Two

---
+++ #2_Practice/#2.7 DOM - If else - Function practice Two/index.js
@@ -0,0 +1,36 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    if (hasClass) {
+        title.classList.remove(CLICKED_CLASS);
+    } else {
+        title.classList.add(CLICKED_CLASS);
+    }
+}
+
+function init(){
+    title.addEventListener("click", handleClick);
+}
+init();
+
+/*
+
+toggle
+
+title.classList.toggle(CLICKED_CLASS);
+
+이전에 했던걸 모두 똑같이 해줌
+
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    if (hasClass) {
+        title.classList.remove(CLICKED_CLASS);
+    } else {
+        title.classList.add(CLICKED_CLASS);
+    }
+
+    -> title.classList.toggle(CLICKED_CLASS);
+
+*/
