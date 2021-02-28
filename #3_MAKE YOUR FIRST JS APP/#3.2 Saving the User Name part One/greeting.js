0a213ebecf68a7daeb155eed3f58f0dd6138503e
HANSOL <yhansol145@gmail.com>
2021-02-28 18:16:51 +0900

    #3.2 Saving the User Name part One

--- #3_MAKE YOUR FIRST JS APP/#3.2 Saving the User Name part One/greeting.js
+++ #3_MAKE YOUR FIRST JS APP/#3.2 Saving the User Name part One/greeting.js
@@ -1,5 +1,27 @@
-function init() {
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
 
-init():
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // she is not
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
